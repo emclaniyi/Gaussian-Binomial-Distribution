426c334a7a074e217e4a6aaac1ef6c5e5c5077c0
Emerald Mclaniyi <emclaniyi@gmail.com>
2020-06-27 01:29:34 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='my_gnb_distributions',
+      version='1.5',
+      description='Gaussian and Binomial distributions',
+      packages=['my_gnb_distributions'],
+      author='Enerald Mclaniyi',
+      author_email = 'emclaniyi@gmail.com',
+     zip_safe=False)
